f9dd9f67f1f774d635875d921c0386c891f69597
GootySohail <109505341+GootySohail@users.noreply.github.com>
2024-06-12 14:26:51 +0530

    Add files via upload

---
+++ sample.py
@@ -0,0 +1,12 @@
+Family = ["Sohail","Humera","Ammi","Abba"]
+
+def print_oddeven(n):
+    if(n % 2 == 0):
+        a = "Even"
+    else:
+        a = "Odd"
+    return a
+
+output = print_oddeven(6)
+
+print(output)
